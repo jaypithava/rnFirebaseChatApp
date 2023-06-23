b387b55bfe0fdd893c779388a61da11294b4ccf7
Jay Pithava <js106085@gmail.com>
2023-06-23 10:25:22 +0530

    user listing added

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,12 +1,74 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import React, {useState} from 'react';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Home from '../tabs/Home';
+import Users from '../tabs/Users';
 
 const Main = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
-    <View>
-      <Text>Main</Text>
+    <View style={styles.container}>
+      {selectedTab == 0 ? <Home /> : <Users /> }
+      <View style={styles.bottomTab}>
+        <TouchableOpacity
+          style={styles.tabStyle}
+          onPress={() => {
+            setSelectedTab(0);
+          }}>
+          <Image
+            source={require('../Assets/Group.png')}
+            style={[
+              styles.tabIcon,
+              {tintColor: selectedTab == 0 ? 'white' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.tabStyle}
+          onPress={() => {
+            setSelectedTab(1);
+          }}>
+          <Image
+            source={require('../Assets/user.png')}
+            style={[
+              styles.tabIcon,
+              {tintColor: selectedTab == 1 ? 'white' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Main;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  bottomTab: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 70,
+    borderRadius: 40,
+    backgroundColor: 'purple',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginBottom: 3,
+  },
+  tabStyle: {
+    width: '50%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabIcon: {
+    width: 60,
+    height: 30,
+    resizeMode: 'center',
+  },
+});
---
+++ src/tabs/Home.js
@@ -0,0 +1,107 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  FlatList,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Loader from '../components/Loader';
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    let tempData = [];
+    setVisible(true);
+    const email = await AsyncStorage.getItem('Email');
+    firestore()
+      .collection('users')
+      .where('email', '!=', email)
+      .get()
+      .then(res => {
+        if (res.docs != []) {
+          setVisible(false);
+          res.docs.map(item => {
+            tempData.push(item.data());
+          });
+          setUsers(tempData);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Firebase Chat App</Text>
+      </View>
+      <FlatList
+        data={users}
+        renderItem={({item, index}) => {
+          return (
+            <View style={styles.userItems}>
+              <Image
+                source={require('../Assets/user.png')}
+                style={styles.userLogo}
+              />
+              <Text style={styles.itemName}>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
+      <Loader visible={visible} />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    width: '100%',
+    height: 50,
+    elevation: 10,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'purple',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  userItems: {
+    width: Dimensions.get('window').width - 50,
+    height: 60,
+    alignSelf: 'center',
+    alignItems: 'center',
+    paddingLeft: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    borderWidth: 0.5,
+    borderRadius: 10,
+  },
+  userLogo: {
+    width: 40,
+    height: 40,
+  },
+  itemName: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '600',
+    marginLeft: 10,
+  },
+});
